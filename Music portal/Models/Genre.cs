86dc826314612b1897d9f431ca1d1ae7bb8ddcf7
IrynaKonovalowa <iri.moummi@gmail.com>
2024-04-02 09:42:47 +0200

    # Добавлен класс Genre

---
+++ Music portal/Models/Genre.cs
@@ -0,0 +1,9 @@
+﻿namespace Music_portal.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Song>? Songs { get; set; }
+    }
+}
