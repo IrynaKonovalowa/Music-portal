1a0e6df3d62ac6ea9feef79128170938f69ccb46
IrynaKonovalowa <iri.moummi@gmail.com>
2024-04-02 09:43:16 +0200

    # Добавлен класс Singer

---
+++ Music portal/Models/Singer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Music_portal.Models
+{
+    public class Singer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Song>? Songs { get; set;}
+    }
+}
