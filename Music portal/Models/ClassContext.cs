433e295d150a78407bfee8d5a63ca2c64283a461
IrynaKonovalowa <iri.moummi@gmail.com>
2024-04-02 09:47:23 +0200

    # Изменены класс Контекст и файл конфигурации

--- Music portal/Models/ClassContext.cs
+++ Music portal/Models/ClassContext.cs
@@ -6,7 +6,11 @@ namespace Music_portal.Models
     public class ClassContext: DbContext
     {
         public DbSet<User> Users { get; set; }
-                
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Singer> Singers { get; set; }
+        public DbSet<Song> Songs { get; set; }
+
+
         public ClassContext(DbContextOptions<ClassContext> options)
             : base(options)
         {
