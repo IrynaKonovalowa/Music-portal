cf12ba0ff15a9c12d07a0f5c3ad223e82a189c02
IrynaKonovalowa <iri.moummi@gmail.com>
2024-04-02 06:41:28 +0200

    # Добавлены  IUserRepository и  UserRepository

---
+++ Music portal/Repository/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Music_portal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Music_portal.Repository
+{
+    public interface IUserRepository
+    {        
+        Task<List<User>> GetUserList();
+        Task <User> GetUser(string login);
+        Task CreateUs(User item);
+        Task Save();
+    }
+}
---
+++ Music portal/Repository/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Music_portal.Models;
+using System.Numerics;
+
+namespace Music_portal.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ClassContext _context;
+
+        public UserRepository(ClassContext context)
+        {
+            _context = context;
+        }        
+        
+        public async Task<List<User>> GetUserList()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task <User> GetUser(string login)
+        {
+            var usersContext = _context.Users.Where(m => m.Login == login);
+
+            return await usersContext.FirstAsync();
+        }
+
+        public async Task CreateUs(User u)
+        {
+            await _context.Users.AddAsync(u);
+        }
+
+        public async Task Save()
+        {
+           await _context.SaveChangesAsync();
+        }       
+    }
+}
