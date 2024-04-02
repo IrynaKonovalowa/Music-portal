5aa04a4aa49026bf3dc637abe282a951da82dff6
IrynaKonovalowa <iri.moummi@gmail.com>
2024-04-02 06:19:58 +0200

    # Добавлен класс Context

---
+++ Music portal/Models/ClassContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Music_portal.Models
+{
+    public class ClassContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+                
+        public ClassContext(DbContextOptions<ClassContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
