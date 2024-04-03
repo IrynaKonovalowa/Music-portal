50c21439b2923b25955eec94c5f6cbee6d955480
IrynaKonovalowa <iri.moummi@gmail.com>
2024-04-03 20:10:25 +0200

    # Добавлен IGenreRepository и GenreRepository

---
+++ Music portal/Repository/GenreRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Music_portal.Models;
+using System.Numerics;
+
+namespace Music_portal.Repository
+{
+    public class GenreRepository : IGenreRepository
+    {
+        private readonly ClassContext _context;
+
+        public GenreRepository(ClassContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Genre>> GetGenreList()
+        {
+            var genreContext = _context.Genres;
+            return await genreContext.ToListAsync();
+        }
+
+        public async Task<Genre> GetGenre(int id)
+        {
+            return await _context.Genres.FindAsync(id);
+        }
+        public async Task CreateGenre(Genre g)
+        {
+            await _context.Genres.AddAsync(g);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
---
+++ Music portal/Repository/IGenreRepository .cs
@@ -0,0 +1,13 @@
+﻿using Music_portal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Music_portal.Repository
+{
+    public interface IGenreRepository
+    {
+        Task<List<Genre>> GetGenreList();
+        Task<Genre> GetGenre(int id);
+        Task CreateGenre(Genre item);
+        Task Save();
+    }
+}
