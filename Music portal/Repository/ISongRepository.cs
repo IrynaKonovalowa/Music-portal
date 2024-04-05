9818dbc4148ee3900a4602cc950478bc252d1a3f
IrynaKonovalowa <iri.moummi@gmail.com>
2024-04-05 21:03:34 +0200

    # Добавлены ISongRepository и SongRepository

---
+++ Music portal/Repository/ISongRepository.cs
@@ -0,0 +1,13 @@
+﻿using Music_portal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Music_portal.Repository
+{
+    public interface ISongRepository
+    {
+        Task<List<Song>> GetSongList();
+        Task<Song> GetSong(int id);
+        Task CreateSong(Song item);
+        Task Save();
+    }
+}
---
+++ Music portal/Repository/SongRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Music_portal.Models;
+using System.Numerics;
+
+namespace Music_portal.Repository
+{
+    public class SongRepository : ISongRepository
+    {
+        private readonly ClassContext _context;
+
+        public SongRepository(ClassContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Song>> GetSongList()
+        {
+            var songContext = _context.Songs;
+            return await songContext.ToListAsync();
+        }
+
+        public async Task<Song> GetSong(int id)
+        {
+            return await _context.Songs.FindAsync(id);
+        }
+        public async Task CreateSong(Song s)
+        {
+            await _context.Songs.AddAsync(s);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
