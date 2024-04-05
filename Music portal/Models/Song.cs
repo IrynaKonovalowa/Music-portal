f72a5234de67f9bd043dcf3d2717b5d9cf99baed
IrynaKonovalowa <iri.moummi@gmail.com>
2024-04-05 21:02:07 +0200

    # Дополнен класс Song

--- Music portal/Models/Song.cs
+++ Music portal/Models/Song.cs
@@ -1,16 +1,23 @@
-﻿namespace Music_portal.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Music_portal.Models
 {
     public class Song
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-
+        [Required(ErrorMessage = "Field must be set!")]
+        public string? Title { get; set; }
+        [Required(ErrorMessage = "Field must be set!")]
         public int Year{ get; set; }
+        
+        public string? PathToFile { get; set; }
+        
+        public int GenreId {  get; set; }
 
-        public string PathToFile { get; set; }
-
+        public int SingerId { get; set; }
+        
         public virtual Genre? Genre{ get; set; }
-
+        
         public virtual Singer? Singer { get; set; }
 
         public virtual User? User { get; set; }
