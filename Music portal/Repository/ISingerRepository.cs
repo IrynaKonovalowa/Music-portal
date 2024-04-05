10f3d4d8f0924425240b812d09c380dbf252cf42
IrynaKonovalowa <iri.moummi@gmail.com>
2024-04-05 20:51:34 +0200

    # Добавлены ISingerRepository and SingerRepository

---
+++ Music portal/Repository/ISingerRepository.cs
@@ -0,0 +1,14 @@
+﻿using Music_portal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Music_portal.Repository
+{
+    public interface ISingerRepository
+    {
+        Task<List<Singer>> GetSingerList();
+        public List<Singer> Singers();
+        Task<Singer> GetSinger(int id);
+        Task CreateSinger(Singer item);
+        Task Save();
+    }
+}
---
+++ Music portal/Repository/SingerRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Music_portal.Models;
+using System.Numerics;
+
+namespace Music_portal.Repository
+{
+    public class SingerRepository : ISingerRepository
+    {
+        private readonly ClassContext _context;
+
+        public SingerRepository(ClassContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Singer>> GetSingerList()
+        {
+            var singerContext = _context.Singers;
+            return await singerContext.ToListAsync();
+        }
+        public List<Singer> Singers()
+        {
+            var singerContext = _context.Singers;
+            return singerContext.ToList();
+        }
+
+        public async Task<Singer> GetSinger(int id)
+        {
+            return await _context.Singers.FindAsync(id);
+        }
+
+        public async Task CreateSinger(Singer g)
+        {
+            await _context.Singers.AddAsync(g);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
