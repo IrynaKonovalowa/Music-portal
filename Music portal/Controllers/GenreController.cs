c89d5280edbbd6a68b1e4c5f02f27e6937fc288d
IrynaKonovalowa <iri.moummi@gmail.com>
2024-04-07 14:41:51 +0200

    # Дополнен GenreController

--- Music portal/Controllers/GenreController.cs
+++ Music portal/Controllers/GenreController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Music_portal.Models;
 using Music_portal.Repository;
 using System.Diagnostics;
@@ -38,5 +39,90 @@ namespace Music_portal.Controllers
             }
             return View(genre);
         }
-    }
+        public async Task<IActionResult> EditGenre(int? id)
+        {
+            if (id == null || await repo.GetGenreList() == null)
+            {
+                return NotFound();
+            }
+            var genre = await repo.GetGenre((int)id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+            return View(genre);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditGenre(int id, [Bind("Id,Name,Songs")] Genre genre)
+        {
+            if (id != genre.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    repo.Update(genre);
+                    await repo.Save();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!await GenreExists(genre.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Genres));
+            }
+            return View(genre);
+        }
+
+        private async Task<bool> GenreExists(int id)
+        {
+            List<Genre> list = await repo.GetGenreList();
+            return (list?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+		public async Task<IActionResult> DeleteGenre(int? id)
+		{
+			if (id == null || await repo.GetGenreList() == null)
+			{
+				return NotFound();
+			}
+
+			var genre = await repo.GetGenre((int)id);
+			if (genre == null)
+			{
+				return NotFound();
+			}
+
+			return View(genre);
+		}
+
+		[HttpPost, ActionName("DeleteGenre")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			if (await repo.GetGenreList() == null)
+			{
+				return Problem("Entity set 'ClassContext.Genres'  is null.");
+			}
+			var genre = await repo.GetGenre(id);
+			if (genre != null)
+			{
+				await repo.Delete(id);
+			}
+			await repo.Save();
+			return RedirectToAction(nameof(Genres));
+		}
+
+	}
 }
